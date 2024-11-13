aabb01bd1c89cf6e96997717772122214541ef75
Joko Santoso <joko74479@gmail.com>
2024-11-13 12:16:52 +0700

    learn: make:migration

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,16 +3,25 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1"
+  >
   @vite('resources/css/app.css')
 
   <title>Laravel</title>
 
   <!-- Fonts -->
-  <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+  <link
+    rel="stylesheet"
+    href="https://rsms.me/inter/inter.css"
+  >
 
   <!-- Alpine -->
-  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+  <script
+    defer
+    src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"
+  ></script>
 </head>
 
 <body>
